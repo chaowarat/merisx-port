f4a9bb931bd85376c29151e384e45176256605f5
chaowarat <chaowaratt@gmail.com>
2018-10-19 12:41:51 +0700

    Updates

---
+++ precache-manifest.29d63457eb5b54696b33b94fe7870ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/merisx-port/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ced2802445248ef21d48",
+    "url": "/merisx-port/static/js/runtime~main.ced28024.js"
+  },
+  {
+    "revision": "fd2c3578b9a8316841ed",
+    "url": "/merisx-port/static/js/main.fd2c3578.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/merisx-port/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "fd2c3578b9a8316841ed",
+    "url": "/merisx-port/static/css/main.aa1e68ac.chunk.css"
+  },
+  {
+    "revision": "4656ee3ad31c1652224395e289d74d33",
+    "url": "/merisx-port/index.html"
+  }
+];
